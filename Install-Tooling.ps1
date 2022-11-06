27f4f60c583e6392ae2d3811abd94c880d950efc
Robert Dailey <github@rdailey.me>
2022-11-06 08:56:33 -0600

    wip: Add commitlint

--- Install-Tooling.ps1
+++ Install-Tooling.ps1
@@ -1,2 +1,7 @@
+# Required for running `Prepare-Release.ps1`
 dotnet tool update --global GitVersion.Tool
 Install-Module -Name ChangelogManagement
+
+# Required for commitlint and other tooling provided by Node
+npm install -g yarn
+yarn install
